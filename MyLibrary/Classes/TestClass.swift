b8e233a9b66f1685176c242abe58e676785584ef
Trekele Smith <trekelesmith@Trekeles-Mac-mini.local>
2019-05-25 15:46:20 -0400

    initial commit

---
+++ MyLibrary/Classes/TestClass.swift
@@ -0,0 +1,14 @@
+//
+//  TestClass.swift
+//  MyLibrary
+//
+//  Created by Trekele Smith on 5/25/19.
+//
+
+import UIKit
+
+public class TestClass: NSObject {
+    public static func GetString() -> String {
+        return "This is a temp string";
+    }
+}
